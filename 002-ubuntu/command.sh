a05ee486fc105af0f0ff11595360520efa72434b
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-07-20 18:00:24 -0400

    ok

--- 002-ubuntu/command.sh
+++ 002-ubuntu/command.sh
@@ -6,3 +6,10 @@ docker run -it ubuntu:latest /bin/bash
 -> cat /etc/os-release
 
 docker start container_name 
+
+
+// this container after exit container will die
+docker run -it ubuntu:latest /bin/bash
+
+// for solve we should use -d flag
+docker run -it -d ubuntu:latest /bin/bash

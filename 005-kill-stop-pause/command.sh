d5cab7488bfb572ac796333ff7f14f7701dc7678
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-07-20 18:31:23 -0400

    kill stop start pause unpause

---
+++ 005-kill-stop-pause/command.sh
@@ -0,0 +1,10 @@
+docker kill run_container_name  // container kill fastly
+
+docker start killed_container_name
+
+docker stop run_container_name // container stop gradully
+
+docker start killed_container_name
+
+docker pause run_container_name // pause  run container
+docker unpause run_container_name // unpause  run container

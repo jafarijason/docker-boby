13350f1d5f79b4b156851a842bc08c78121fcf35
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-07-20 17:57:21 -0400

    part1

---
+++ 002-ubuntu/command.sh
@@ -0,0 +1,8 @@
+docker pull ubuntu:latest
+
+docker run -it ubuntu:latest /bin/bash
+
+-> inside of containe r for test  cat proc/cpuinfo  | grep core
+-> cat /etc/os-release
+
+docker start container_name 

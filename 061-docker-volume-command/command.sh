c47a6b45543fde75cad7f82e1e731d3843a1353e
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-07-20 23:22:35 -0400

    docker volume works

---
+++ 061-docker-volume-command/command.sh
@@ -0,0 +1,8 @@
+docker volume create hello_world
+docker volume create hello_world2
+docker volume ls
+
+docker volume inspect hello_world
+
+docker volume rm  VOLUME_ID_OR_NAME
+docker volume prune 

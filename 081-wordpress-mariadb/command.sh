226d5fe79780ea40513fd7512d0df1133446dbe1
thisisjasonjafari <jason.jafari.m@gmail.com>
2020-07-21 00:42:43 -0400

    **********wordpress done **********

--- 081-wordpress-mariadb/command.sh
+++ 081-wordpress-mariadb/command.sh
@@ -1,1 +1,7 @@
-  s
+docker pull wordpress:latest
+
+docker pull mariadb:latest
+
+docker run -d -e MYSQL_DATABASE=wordpress -e MYSQL_ROOT_PASSWORD=1234566 --name wordpressdb -v "$(pwd)/database":/var/lib/mysql  mariadb:latest
+
+docker run -d -e WORDPRESS_DB_PASSWORD=1234566  --name wordpressweb --link  wordpressdb:mysql -p 8080:80 -v $(pwd)/wordpressweb:/var/www/html wordpress:latest
